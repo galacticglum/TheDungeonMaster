cbe8e602890a64cc9f67eb84515527c9f878e041
GalacticGlum <nerftactic09@gmail.com>
2018-01-21 15:49:47 -0500

    Added initialization logic component.

---
+++ Assets/Game/Scripts/InitializationLogic.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Author: Shon Verch
+ * File Name: InitializationLogic.cs
+ * Project Name: TheDungeonMaster
+ * Creation Date: 01/21/2018
+ * Modified Date: 01/21/2018
+ * Description: A component which exposes a initialization event to the editor.
+ */
+
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// A component which exposes a initialization event to the editor.
+/// </summary>
+public class InitializationLogic : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent onStart;
+
+    private void Start() => onStart.Invoke();
+}
